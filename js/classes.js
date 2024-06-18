f8da99582a5646871c127b318f3a5272963204fa
RichardCastellano <richyrich3790@gmail.com>
2024-06-17 18:50:34 -0700

    added a default template for the Month class

--- js/classes.js
+++ js/classes.js
@@ -1,6 +1,58 @@
 class Month {
   constructor() {
-    this.categories = [];
+    this.categories = [
+      {
+        name: "Income",
+        items: [
+          { name: "Paycheck 1", amount: 0 },
+          { name: "Paycheck 2", amount: 0 },
+        ],
+      },
+      {
+        name: "Savings",
+        items: [{ name: "Emergency Fund", amount: 0 }],
+      },
+      {
+        name: "Housing",
+        items: [
+          { name: "Mortgage/Rent", amount: 0 },
+          { name: "Natural Gas", amount: 0 },
+          { name: "Electricity", amount: 0 },
+          { name: "Internet", amount: 0 },
+        ],
+      },
+      {
+        name: "Transportation",
+        items: [{ name: "Gas", amount: 0 }],
+      },
+      {
+        name: "Food",
+        items: [{ name: "Groceries", amount: 0 }],
+      },
+      {
+        name: "Personal",
+        items: [
+          { name: "Phone", amount: 0 },
+          { name: "Spotify", amount: 0 },
+          { name: "Netflix", amount: 0 },
+        ],
+      },
+      {
+        name: "Lifestyle",
+        items: [
+          { name: "Clothes", amount: 0 },
+          { name: "Pet Care", amount: 0 },
+        ],
+      },
+      {
+        name: "Insurance",
+        items: [
+          { name: "Auto Insurance", amount: 0 },
+          { name: "Home Insurance", amount: 0 },
+          { name: "Life Insurance", amount: 0 },
+        ],
+      },
+    ];
   }
 
   getCategory(categoryName) {
@@ -13,15 +65,17 @@ class Month {
   }
 
   getSpecficItemInACategory(categoryName, itemName) {
-     return this.getCategory(categoryName).items.find(item => item.name === itemName)
+    return this.getCategory(categoryName).items.find(
+      (item) => item.name === itemName
+    );
   }
 
   addCategory(categoryName) {
     const category = {
       name: categoryName,
-      items: []
-    }
-    this.categories.push(category)
+      items: [],
+    };
+    this.categories.push(category);
   }
 
   addItemToCategory(categoryName, itemName) {
@@ -34,30 +88,33 @@ class Month {
   }
 
   deleteCategory(categoryName) {
-    this.categories = this.categories.filter(cat => cat.name !== categoryName)
+    this.categories = this.categories.filter(
+      (cat) => cat.name !== categoryName
+    );
   }
-  
+
   deleteItemInCategory(categoryName, itemName) {
-    const category = this.getCategory(categoryName)
-    category.items = this.getItemsInACategory(categoryName).filter(item => item.name !== itemName)
+    const category = this.getCategory(categoryName);
+    category.items = this.getItemsInACategory(categoryName).filter(
+      (item) => item.name !== itemName
+    );
   }
 
   deletAllCategories() {
-    this.categories = []
+    this.categories = [];
   }
-  
+
   deleteAllItemsInACategory(categoryName) {
-    const category = this.getCategory(categoryName)
-    category.items = []
+    const category = this.getCategory(categoryName);
+    category.items = [];
   }
 
   updateAmountValue(categoryName, itemName, newAmount) {
-    const item = this.getSpecficItemInACategory(categoryName, itemName)
-    item.amount = newAmount
+    const item = this.getSpecficItemInACategory(categoryName, itemName);
+    item.amount = newAmount;
   }
 }
 
-
 // handle errors like check for duplicates on categories and items
-
-
+let june = new Month();
+console.log(june);
