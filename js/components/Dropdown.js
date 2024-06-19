45ea4a33c94d909a3a0d1ee901d584e76a271f19
RichardCastellano <richyrich3790@gmail.com>
2024-06-18 23:10:25 -0700

    reorganized the file structure

--- js/components/Dropdown.js
+++ js/components/Dropdown.js
@@ -1,23 +1,4 @@
-import { Month } from "./classes.js";
-
-const years = [
-  2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030,
-];
-
-const months = [
-  "Janurary",
-  "Feburary",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+import {months, years} from '../utils/constants.js'
 
 for (let i = 0; i < years.length; i++) {
   let year = years[i];
---
+++ js/utils/constants.js
@@ -0,0 +1,18 @@
+export const years = [
+    2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030
+  ];
+  
+  export const months = [
+    "Janurary",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
