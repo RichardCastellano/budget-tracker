d9444a08af1b0418f86cabebd2723624459d64c9
Richard Castellano <richardcastellano@Gabys-Air.lan>
2024-06-24 00:14:40 -0700

    added a function that adds a categorysection to the html file

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
-import { initializeDropdown } from "./components/dropdown.js";
-import { userInput, postTemplate } from "./components/BudgetTemplate.js";
 import { createCategory } from "./utils/functions.js";
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  
-
   createCategory()
 });
 
--- js/utils/functions.js
+++ js/utils/functions.js
@@ -1,29 +1,29 @@
-const structureItemLine = () => {
-    const itemInfo = document.createElement('div')
-    const itemName = document.createElement('input')
-    const itemAmount = document.createElement('input')
-    itemInfo.setAttribute('class', 'item-info')
-    itemName.setAttribute('class', 'item')
-    itemAmount.setAttribute('class', 'amount')
-    itemInfo.append(itemName)
-    itemInfo.append(itemAmount)
-    return itemInfo
+const createItemLine = () => {
+    const itemInfo = document.createElement('div');
+    const itemName = document.createElement('input');
+    const itemAmount = document.createElement('input');
+    itemInfo.setAttribute('class', 'item-info');
+    itemName.setAttribute('class', 'item');
+    itemAmount.setAttribute('class', 'amount');
+    itemInfo.append(itemName);
+    itemInfo.append(itemAmount);
+    return itemInfo;
 }
 
 const createAddItemBtn = () => {
-    const addItemBtn = document.createElement('button')
-    addItemBtn.setAttribute('class', 'add-item-btn')
-    addItemBtn.textContent = 'Add Item'
-    return addItemBtn
+    const addItemBtn = document.createElement('button');
+    addItemBtn.setAttribute('class', 'add-item-btn');
+    addItemBtn.textContent = 'Add Item';
+    return addItemBtn;
 }
 
-export const createCategory = () => {
-    const categoryInfo = document.createElement('div')
-    const categoryName = document.createElement('input')
-    const addItemBtn = createAddItemBtn()
-    categoryName.setAttribute('id', categoryName.value)
-    categoryName.append(addItemBtn)
-    categoryInfo.append(categoryName)
+export const createCategorySection = () => {
+    const categoryInfo = document.createElement('div');
+    const categoryName = document.createElement('input');
+    const addItemBtn = createAddItemBtn();
+    categoryInfo.append(categoryName);
+    categoryInfo.append(addItemBtn);
+    return categoryInfo;
 }
 
 
