063dcb160d5b54cbebff5e7a89422669435fee5a
RichardCastellano <richyrich3790@gmail.com>
2024-07-01 22:18:42 -0700

    added mongoDB

--- js/components/Months-scroll-btn.js
+++ js/components/Months-scroll-btn.js
@@ -2,7 +2,7 @@ import { months } from "../utils/constants.js";
 
 // Gets current month that we are in right now 
 export const getCurrentMonth = () => {
-    const monthElement = document.getElementById('month')
+    const monthElement = document.getElementById('month-name')
     let date = new Date()
     let index = date.getMonth()
     monthElement.innerHTML = months[index]
@@ -14,7 +14,7 @@ export const nextBtnPressed = () => {
     index = (index + 1) % months.length; // Wrap around to the beginning if end is reached
     updateMonth(index);
 }
-// Fucntion when previous btn is pressed 
+// Function when previous btn is pressed 
 export const previousBtnPressed = () => {
     let index = getCurrentIndex();
     index = (index - 1 + months.length) % months.length; // Wrap around to the end if beginning is reached
@@ -23,13 +23,13 @@ export const previousBtnPressed = () => {
 
 // Gets the current index # of the month that is currently displayed 
 const getCurrentIndex = () => {
-    const monthElement = document.getElementById('month');
+    const monthElement = document.getElementById('month-name');
     const currentMonth = monthElement.innerHTML.trim();
     return months.indexOf(currentMonth);
 }
 
 // Updates the the monthElement to display the new month
 const updateMonth = (index) => {
-    const monthElement = document.getElementById('month');
+    const monthElement = document.getElementById('month-name');
     monthElement.innerHTML = months[index];
 }
